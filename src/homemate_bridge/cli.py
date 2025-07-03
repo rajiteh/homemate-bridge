1668488408adff8e0714a76f036e2c68244c71ee
Raj Perera <rajiteh@gmail.com>
2025-07-02 20:58:14 -0400

    feat: improve mqtt device info reporting

--- src/homemate_bridge/cli.py
+++ src/homemate_bridge/cli.py
@@ -37,18 +37,60 @@ CMD_SERVER_SENDS = [15]
 
 class HomemateSwitch(Switch):
 
-    def __init__(self, handler, *args, **kwargs):
+    def __init__(self, handler, unique_id, **kwargs):
         self._handler = handler
-        super().__init__(*args, **kwargs)
+        self.unique_id = unique_id
+        super().__init__(**kwargs)
 
+    @Switch.state.setter
+    def state(self, value):
+        Switch.state.fset(self, value)
+        self.client.publish(self.state_topic, value, retain=self.retain)
+
+    @property
+    def config(self):
+        return {
+            'name': "Power",
+            'state_topic': self.state_topic,
+            'command_topic': self.command_topic,
+            'payload_on': self.payload_on,
+            'payload_off': self.payload_off,
+            'retain': self.retain,
+            'unique_id': self.unique_id + "_switch",
+            'device': {
+                'identifiers': [self.unique_id],
+                'name': self.name,
+                'manufacturer': "HOMEMATE",
+                'model': "HOMEMATE Switch",
+            }
+        }
+    
     def on_state_change(self, new_state):
         logger.debug("Setting new state: {}".format(new_state))
         self._handler.order_state_change(new_state == self.payload_on)
 
 class HomematePowerSensor(Sensor):
-    def __init__(self, handler, *args, **kwargs):
+    def __init__(self, handler, unique_id, **kwargs):
         self._handler = handler
-        super().__init__(*args, **kwargs)
+        self.unique_id = unique_id
+        super().__init__(**kwargs)
+
+    @property
+    def config(self):
+        return {
+            'name': "Power",
+            'state_topic': self.state_topic,
+            'state_class': 'measurement',
+            'device_class': 'energy',
+            'unit_of_measurement': 'W',
+            'unique_id': self.unique_id + "_power",
+            'device': {
+                'identifiers': [self.unique_id],
+                'name': self.name,
+                'manufacturer': "HOMEMATE",
+                'model': "HOMEMATE Switch",
+            }
+        }
 
     def on_energy_usage_change(self, energy_reading):
         logger.debug("Setting new power output: {}".format(energy_reading))
@@ -324,20 +366,8 @@ class HomemateTCPHandler(socketserver.BaseRequestHandler):
                     self._mqtt_switch = HomemateSwitch(
                         self,
                         name=self.settings['name'],
-                        entity_id=self.entity_id
-                    )
-                    # Sanity check: Does our own packet look valid?
-                    #HomematePacket(response_packet, self.keys)
-                    self.request.sendall(response_packet)
-
-                if self._mqtt_switch is None and packet.json_payload['cmd'] == 32:
-                    # Setup the mqtt connection once we see the initial state update
-                    # Otherwise, we will get the previous state too early
-                    # and the switch will disconnect when we try to update it
-                    self._mqtt_switch = HomemateSwitch(
-                        self,
-                        name=self.settings['name'],
-                        entity_id=self.client_address[0].replace('.', '_')
+                        entity_id=self.entity_id,
+                        unique_id=self.switch_id.decode('utf-8'),
                     )
                     self.__class__._broker.add_device(self._mqtt_switch)
 
@@ -451,7 +481,8 @@ class HomemateTCPHandler(socketserver.BaseRequestHandler):
                 if self._mqtt_sensor is None:
                     self._mqtt_sensor = HomematePowerSensor(self,
                         name=self.settings['name'],
-                        entity_id=self.client_address[0].replace('.', '_')
+                        entity_id=self.entity_id,
+                        unique_id=self.switch_id.decode('utf-8'),
                     )
                 self.__class__._broker.add_device(self._mqtt_sensor)
 
